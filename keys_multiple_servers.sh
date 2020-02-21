bc34d948f1204cf0132171d98f23fbb652dc525c
sfenman <stownerakos@gmail.com>
2020-02-21 17:34:10 +0200

    delete printf

--- keys_multiple_servers.sh
+++ keys_multiple_servers.sh
@@ -11,7 +11,5 @@ read -ra source_key
 
 for ((i=0;i<${#servers[@]};++i)); do
     sshpass -p "${passwords[i]}" ssh ${servers[i]} mkdir -p '$HOME/.ssh/'
-
-    printf "%s is in %s\n" "${servers[i]}" "${passwords[i]}"
     sshpass -p "${passwords[i]}" ssh-copy-id -i $source_key ${servers[i]}
 done 
